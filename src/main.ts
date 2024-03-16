39fd0b143521415329c0a08d96644de56b63ea7d
Emanuel Bacalhau <carlosemanuelbatistabacalhau@gmail.com>
2024-03-16 00:07:10 -0300

    feat: finish logic for pagination and setup for test od teacher

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,24 @@
 import { Strategy } from './memory/Strategy'
+import { Process } from './process/Process'
 import { SystemCallType } from './so/SystemCallType'
 import { SystemOperation } from './so/SystemOperation'
 
 // Examples
-const systemOperation = new SystemOperation(Strategy.PAGING)
+const systemOperation = new SystemOperation(Strategy.FIRST_FIT)
 
-const p1 = systemOperation.systemCall(SystemCallType.OPEN_PROCESS)
-systemOperation.systemCall(SystemCallType.WRITE_PROCESS, p1)
+const p2 = new Process(38)
 
-const p2 = systemOperation.systemCall(SystemCallType.OPEN_PROCESS)
+// const p1 = systemOperation.systemCall(SystemCallType.OPEN_PROCESS)
+systemOperation.systemCall(SystemCallType.WRITE_PROCESS, new Process(20))
+
 systemOperation.systemCall(SystemCallType.WRITE_PROCESS, p2)
 
-// const p3 = systemOperation.systemCall(SystemCallType.OPEN_PROCESS)
-// systemOperation.systemCall(SystemCallType.WRITE_PROCESS, p3)
+systemOperation.systemCall(SystemCallType.WRITE_PROCESS, new Process(38))
 
-// const p4 = systemOperation.systemCall(SystemCallType.OPEN_PROCESS)
-// systemOperation.systemCall(SystemCallType.WRITE_PROCESS, p4)
+systemOperation.systemCall(SystemCallType.WRITE_PROCESS, new Process(20))
 
-systemOperation.systemCall(SystemCallType.CLOSE_PROCESS, p1)
-// systemOperation.systemCall(SystemCallType.CLOSE_PROCESS, p2)
-// systemOperation.systemCall(SystemCallType.CLOSE_PROCESS, p4)
+systemOperation.systemCall(SystemCallType.CLOSE_PROCESS, p2)
 
-// const p5 = systemOperation.systemCall(SystemCallType.OPEN_PROCESS)
-// systemOperation.systemCall(SystemCallType.WRITE_PROCESS, p5)
+systemOperation.systemCall(SystemCallType.WRITE_PROCESS, new Process(8))
+
+// systemOperation.systemCall(SystemCallType.READ_PROCESS, p1)
